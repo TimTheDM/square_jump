620ed4d4426b1af964dd7a3848a0b843954b53bd
Timothy <tbsmithlin@gmail.com>
2021-03-13 21:26:38 -0800

    Added members and constructor for fixture.

--- source/stage_init.cpp
+++ source/stage_init.cpp
@@ -14,3 +14,17 @@ player::player() {
   sf::RectangleShape pSprite(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
   this->pSprite = pSprite;
 }
+
+fixture::fixture(bool isHazard, bool isWinPoint, sf::Texture& hazardText, sf::Vector2f& squareDimension) {
+  this->isHazard = isHazard;
+  this->isWinPoint = isWinPoint;
+
+  if(isHazard) {
+    sf::Sprite placeholder;
+    placeholder.setTexture(hazardText);
+    this->hazard = placeholder;
+  } else {
+    sf::RectangleShape placeholder(squareDimension);
+    this->platform = placeholder;
+  }
+}
--- source/stage_init.h
+++ source/stage_init.h
@@ -3,7 +3,13 @@
 const int PLAYER_SIZE = 50;
 
 class fixture {
-  
+  private:
+    sf::RectangleShape platform;
+    sf::Sprite hazard;
+  public:
+    bool isHazard;
+    bool isWinPoint;
+    fixture(bool, bool, sf::Texture&, sf::Vector2f&);
 };
 
 class player {
