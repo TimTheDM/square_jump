443a42dcfe0ad60beed918df5bd6a87fc2f814ca
Alex Muszynski <Cryatonic@gmail.com>
2021-03-15 01:48:06 -0400

    Updated gravity and collision files with boundary detection variables.

--- source/gravity.cpp
+++ source/gravity.cpp
@@ -3,13 +3,27 @@
 #include "stage_init.h"
 #include "collision.h"
 //Includes functions for changing acceleration while midair
+/*
+The formula for the parabolic jump of the player, in regards to their perspective of the screen, is:
+f(x) = (-j/T^2)(t - T)^2 + j; j = jumpHeight, T = timeToApex, t = time
 
-const double jumpHeight = 120.0; //rough height the bottom edge of the player in pixels
-const double timeToApex = 12.0; //time in game frames to reach the height of the jump
+The formula for the parabolic jump velocity of the player is:
+f'(x) = (-2j/T^2)(t - T)
+
+The formula for the parabolic jump acceleration of the player is:
+f''(x) = -2j/T^2
+
+Due to the y-axis of the window being opposite to standard, with the top being 0 and going down is the positive y direction, a positive acceleration delineates falling on the screen.
+As such, the player sprite is moved in a negative acceleration (-p1.acceleration) to compensate.
+*/
+
+const double jumpHeight = 320.0; //rough height the top edge of the player in pixels
+const double timeToApex = 15.0; //time in game frames to reach the height of the jump
 const double maxFallRate = -2 * (-((2 * jumpHeight)/(timeToApex * timeToApex)) + ((2 * jumpHeight)/timeToApex)); //limits the maximum downward acceleration to twice the normal jump arc
 
 sf::Vector2f position;
 bool pressed = false;
+int boundaryDistance = 0;
 
 void gravity(std::vector<fixture>*, player&, int);
 
@@ -19,17 +33,18 @@ void gravity(std::vector<fixture>* stages, player& p1, int stageOn) {
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !pressed /*jump command was pressed*/)
   {
       pressed = true;
-      p1.acceleration -= (-((2 * jumpHeight)/(timeToApex * timeToApex)) + ((2 * jumpHeight)/timeToApex));
-      p1.pSprite.move(0, p1.acceleration);
+      p1.acceleration += (-((2 * jumpHeight)/(timeToApex * timeToApex)) + ((2 * jumpHeight)/timeToApex));
+      p1.pSprite.move(0, -p1.acceleration);
   }
-  if (!downCollision(stages, p1, stageOn))
+  if (!downCollision(stages, p1, stageOn, boundaryDistance))
   {
-      p1.acceleration -= -((2 * jumpHeight)/(timeToApex * timeToApex));
+      p1.acceleration += -((2 * jumpHeight)/(timeToApex * timeToApex));
       if (p1.acceleration < maxFallRate)
       {
           p1.acceleration = maxFallRate;
       }
-      p1.pSprite.move(0, p1.acceleration);
+      //if (p1.acceleration )
+      p1.pSprite.move(0, -p1.acceleration);
 
       position = p1.pSprite.getPosition();
       if (position.y > 800 - 100)
@@ -39,9 +54,16 @@ void gravity(std::vector<fixture>* stages, player& p1, int stageOn) {
           pressed = false;
       }
   }
-  if (downCollision(stages, p1, stageOn) or aboveCollision(stages, p1, stageOn))
+  if (downCollision(stages, p1, stageOn, boundaryDistance) or aboveCollision(stages, p1, stageOn, boundaryDistance))
   {
       p1.acceleration = 0;
-      p1.pSprite.move(0, p1.acceleration);
+      if (downCollision)
+      {
+          p1.pSprite.move(0, -boundaryDistance);
+      }
+      if (aboveCollision)
+      {
+          p1.pSprite.move(0, boundaryDistance);
+      }
   }
 }
