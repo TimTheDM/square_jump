2ef0b3f760c9a13ea2ffbb4a12a1b07e8accd82f
Timothy <tbsmithlin@gmail.com>
2021-03-16 15:48:28 -0700

    Added boundaryDistance reference to definitions and prototype

--- source/collision.cpp
+++ source/collision.cpp
@@ -4,33 +4,33 @@
 //file for collision detection functions
 
 void stateCheck(std::vector<fixture>*, player&, int);
-bool aboveCollision(std::vector<fixture>*, player&, int);
-bool leftCollision(std::vector<fixture>*, player&, int);
-bool rightCollision(std::vector<fixture>*, player&, int);
-bool downCollision(std::vector<fixture>*, player&, int);
-bool isCollide(std::vector<fixture>*, player&, int);
+bool aboveCollision(std::vector<fixture>*, player&, int, int&);
+bool leftCollision(std::vector<fixture>*, player&, int, int&);
+bool rightCollision(std::vector<fixture>*, player&, int, int&);
+bool downCollision(std::vector<fixture>*, player&, int, int&);
+bool isCollide(std::vector<fixture>*, player&, int, int&);
 
 void stateCheck(std::vector<fixture>* stages, player& p1, int stageOn) {
   //checks if win or hazard object is being touched
   //runs helper functions to change block to starting point
 }
 
-bool aboveCollision(std::vector<fixture>* stages, player& p1, int stageOn) {
+bool aboveCollision(std::vector<fixture>* stages, player& p1, int stageOn, int& boundaryDistance) {
   //checks if colliding fixture is above player object sprite
   return false;
 }
 
-bool leftCollision(std::vector<fixture>* stages, player& p1, int stageOn) {
+bool leftCollision(std::vector<fixture>* stages, player& p1, int stageOn, int& boundaryDistance) {
   //checks if colliding fixture is left of player object sprite
   return false;
 }
 
-bool rightCollision(std::vector<fixture>* stages, player& p1, int stageOn) {
+bool rightCollision(std::vector<fixture>* stages, player& p1, int stageOn, int& boundaryDistance) {
   //checks if colliding fixture is right of player object sprite
   return false;
 }
 
-bool downCollision(std::vector<fixture>* stages, player& p1, int stageOn) {
+bool downCollision(std::vector<fixture>* stages, player& p1, int stageOn, int& boundaryDistance) {
   //checks if colliding fixture is below player object sprite
   return false;
 }
