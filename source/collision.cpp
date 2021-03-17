180e7fd0ebf705ec165531166a4328a5a84ee381
Timothy <tbsmithlin@gmail.com>
2021-03-16 22:56:37 -0700

    aboveCollision programmed, not tested. isAbove prototypes.

--- source/collision.cpp
+++ source/collision.cpp
@@ -5,19 +5,43 @@
 #include "collision.h"
 //file for collision detection functions
 
+bool isRun = true;
 
 void stateCheck(std::vector<fixture>* stages, player& p1, int stageOn) {
   //checks if win or hazard object is being touched
   //runs helper functions to change block to starting point
+  if (isRun) {
+    int guh = 0;
+    aboveCollision(stages, p1, stageOn, guh);
+    isRun = false;
+  }
 }
 
 bool aboveCollision(std::vector<fixture>* stages, player& p1, int stageOn, int& boundaryDistance) {
   
+  sf::FloatRect fixtureRect;
+  sf::FloatRect playerRect = p1.pSprite.getGlobalBounds();;
+
   for (int i = 0;i < stages[stageOn].size();i++) {
-    if (!stages[stageOn].at(i).isHazard) {
-      sf::Vector2f fixturePos = stages[stageOn].at(i).platform.getPosition();
-    }
+
+    if (!stages[stageOn].at(i).isHazard) fixtureRect = stages[stageOn].at(i).platform.getGlobalBounds();
+    else fixtureRect = stages[stageOn].at(i).hazard.getGlobalBounds();
+    
+    if (fixtureRect.top > playerRect.top) {
+      if ((playerRect.left <= fixtureRect.left && (fixtureRect.left - playerRect.left) < 100)  ||
+      (playerRect.left >= fixtureRect.left && (playerRect.left - fixtureRect.left) < fixtureRect.width)) {
+        int distance = playerRect.top - (fixtureRect.top + fixtureRect.height);
+        if (distance < boundaryDistance) {
+          boundaryDistance -= distance;
+          return true;
+        } 
+      }
+    } 
   }
+  return false;
+}
+
+bool isAbove(const sf::FloatRect& fixtureRect, const sf::FloatRect& playerRect) {
   return false;
 }
 
