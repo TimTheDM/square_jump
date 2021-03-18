ae2ee159405e9e8ef8491a13408b7e41e5394d67
Zerminar12 <zerminar12@gmail.com>
2021-03-18 02:11:42 -0400

    Implemented collision into the movement functions.

--- source/movement.cpp
+++ source/movement.cpp
@@ -16,17 +16,17 @@ void pressCheck(std::vector<fixture>* stages, player& p1, int stageOn) {
   //checks what keyboard buttons are being held down, calls sideMove or jump
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
   {
-        leftMove();
+        leftMove(stages, p1, stageOn);
   }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
   {
-        rightMove();
+        rightMove(stages, p1, stageOn);
   }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
   {
-        jump();
+        jump(p1);
   }
 }
 
@@ -35,8 +35,13 @@ void rightMove(std::vector<fixture>*, player& p1, int stageOn)
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Shift))
     {
         p1.pSprite.move(12.f,0.f);
+        rightCollision(stages, p1, stageOn, 12);
     }
-    else{p1.pSprite.move(5.f,0.f);}
+    else
+    {
+        p1.pSprite.move(5.f,0.f);
+        rightCollision(stages, p1, stageOn, 5);
+    }
 }
 
 void leftMove(std::vector<fixture>*, player& p1, int stageOn)
@@ -44,11 +49,17 @@ void leftMove(std::vector<fixture>*, player& p1, int stageOn)
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Shift))
     {
         p1.pSprite.move(-12.f,0.f);
+        leftCollision(stages, p1, stageOn, 12);
     }
-    else{p1.pSprite.move(-5.f,0.f);}
+    else
+    {
+        p1.pSprite.move(-5.f,0.f);
+        leftCollision(stages, p1, stageOn, 5);
+    }
 }
 
 void jump(player&)
 {
   //sets player objects acceleration according to Alex's Nonsense
+
 }
