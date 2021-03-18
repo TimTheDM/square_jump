09c7b85a3a43316d7f2ee756a307f0481354262f
Zerminar12 <zerminar12@gmail.com>
2021-03-18 02:43:21 -0400

    Fixed the crashing issue. Tweaked sprint speed (lowered it from 12 pixels to 9). Adjusted parameters and arguments as necessary. Added the movePixels int variable to allow the movement functions to play nice with the collision functions.

--- source/movement.cpp
+++ source/movement.cpp
@@ -10,7 +10,7 @@
 void pressCheck(std::vector<fixture>*, player&, int);
 void leftMove(std::vector<fixture>*, player&, int);
 void rightMove(std::vector<fixture>*, player&, int);
-void jump(player&);
+void jump(std::vector<fixture>*, player&, int);
 
 void pressCheck(std::vector<fixture>* stages, player& p1, int stageOn) {
   //checks what keyboard buttons are being held down, calls sideMove or jump
@@ -26,43 +26,50 @@ void pressCheck(std::vector<fixture>* stages, player& p1, int stageOn) {
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
   {
-        jump(p1);
+        jump(stages, p1, stageOn);
   }
 }
 
-void rightMove(std::vector<fixture>*, player& p1, int stageOn)
+void rightMove(std::vector<fixture>* stages, player& p1, int stageOn)
 {
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Shift))
+    int movePixels = 5;
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
     {
+        movePixels = 9;
         p1.pSprite.move(12.f,0.f);
-        rightCollision(stages, p1, stageOn, 12);
+        rightCollision(stages, p1, stageOn, movePixels);
     }
     else
     {
         p1.pSprite.move(5.f,0.f);
-        rightCollision(stages, p1, stageOn, 5);
+        rightCollision(stages, p1, stageOn, movePixels);
     }
 }
 
-void leftMove(std::vector<fixture>*, player& p1, int stageOn)
+void leftMove(std::vector<fixture>* stages, player& p1, int stageOn)
 {
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Shift))
+    int movePixels = -5;
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
     {
+        int movePixels = -9;
         p1.pSprite.move(-12.f,0.f);
-        leftCollision(stages, p1, stageOn, 12);
+        leftCollision(stages, p1, stageOn, movePixels);
     }
     else
     {
         p1.pSprite.move(-5.f,0.f);
-        leftCollision(stages, p1, stageOn, 5);
+        leftCollision(stages, p1, stageOn, movePixels);
     }
 }
 
-void jump(player&)
+void jump(std::vector<fixture>* stages, player& p1, int stageOn)
 {
     //sets player objects acceleration according to Alex's Nonsense
-    if(downCollision())
+    int boundaryDistance = 10;
+    if(downCollision(stages, p1, stageOn, boundaryDistance))
     {
-        p1.acceleration = 5;
+        p1.acceleration = 10;
     }
 }
