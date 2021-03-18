970277eeccc3b74030488af5b4c1a64b1bf13bb7
Zerminar12 <zerminar12@gmail.com>
2021-03-18 01:41:00 -0400

    Implemented certain ancillary features.

--- source/movement.cpp
+++ source/movement.cpp
@@ -8,36 +8,47 @@
 //calls movement functions, and uses functions from collision
 
 void pressCheck(std::vector<fixture>*, player&, int);
-void sideMove(char, std::vector<fixture>*, player&, int);
+void leftMove(std::vector<fixture>*, player&, int);
+void rightMove(std::vector<fixture>*, player&, int);
 void jump(player&);
 
 void pressCheck(std::vector<fixture>* stages, player& p1, int stageOn) {
   //checks what keyboard buttons are being held down, calls sideMove or jump
   if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
   {
-        p1.pSprite.move(-5.f,0.f);
+        leftMove();
   }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
   {
-        p1.pSprite.move(5.f,0.f);
+        rightMove();
   }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
   {
-        p1.pSprite.move(0.f, -5.f);
-  }
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-  {
-        p1.pSprite.move(0.f, 5.f);
+        jump();
   }
 }
 
-void sideMove(char side, std::vector<fixture>*, player& p1, int stageOn) {
-  //moves to side according to side, checking collision before displacing player
+void rightMove(std::vector<fixture>*, player& p1, int stageOn)
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Shift))
+    {
+        p1.pSprite.move(12.f,0.f);
+    }
+    else{p1.pSprite.move(5.f,0.f);}
 }
 
-void jump(player&) {
-  //sets player objects acceleration according to a const I'll put somewhere
+void leftMove(std::vector<fixture>*, player& p1, int stageOn)
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Shift))
+    {
+        p1.pSprite.move(-12.f,0.f);
+    }
+    else{p1.pSprite.move(-5.f,0.f);}
+}
+
+void jump(player&)
+{
+  //sets player objects acceleration according to Alex's Nonsense
 }
