79ddd2e419d452112a899d124784de333fb5a182
Zerminar12 <zerminar12@gmail.com>
2021-03-18 23:22:01 -0400

    Fixed a potential conflict between movement and collision by passing movePixels as boundaryDistance instead of a constant as boundaryDistance.

--- source/movement.cpp
+++ source/movement.cpp
@@ -37,12 +37,12 @@ void rightMove(std::vector<fixture>* stages, player& p1, int stageOn)
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
     {
         movePixels = 9;
-        p1.pSprite.move(12.f,0.f);
+        p1.pSprite.move(movePixels,0.f);
         rightCollision(stages, p1, stageOn, movePixels);
     }
     else
     {
-        p1.pSprite.move(5.f,0.f);
+        p1.pSprite.move(movePixels,0.f);
         rightCollision(stages, p1, stageOn, movePixels);
     }
 }
@@ -54,12 +54,12 @@ void leftMove(std::vector<fixture>* stages, player& p1, int stageOn)
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
     {
         int movePixels = -9;
-        p1.pSprite.move(-12.f,0.f);
+        p1.pSprite.move(movePixels,0.f);
         leftCollision(stages, p1, stageOn, movePixels);
     }
     else
     {
-        p1.pSprite.move(-5.f,0.f);
+        p1.pSprite.move(movePixels,0.f);
         leftCollision(stages, p1, stageOn, movePixels);
     }
 }
